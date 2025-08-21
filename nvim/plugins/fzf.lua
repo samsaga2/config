7386992e72a14f190d6e3930e6b8ae99f2fce8eb
Victor M. <samsaga2@gmail.com>
2025-08-21 13:45:43 +0200

    Added telescope and gitsigns to nvim

--- nvim/plugins/fzf.lua
+++ nvim/plugins/fzf.lua
@@ -4,10 +4,10 @@ return {
         "https://github.com/junegunn/fzf",
     },
     keys = {
-        { "<Leader><Leader>", "<Cmd>Files<CR>", desc = "Find files" },
-        { "<Leader>fb", "<Cmd>Buffers<CR>", desc = "Find buffers" },
-        { "<Leader>fp", "<Cmd>Rg<CR>", desc = "Search project" },
-        { "<Leader>fr", "<Cmd>History<CR>", desc = "Find recent files" },
+        -- { "<Leader><Leader>", "<Cmd>Files<CR>", desc = "Find files" },
+        -- { "<Leader>fb", "<Cmd>Buffers<CR>", desc = "Find buffers" },
+        -- { "<Leader>fp", "<Cmd>Rg<CR>", desc = "Search project" },
+        -- { "<Leader>fr", "<Cmd>History<CR>", desc = "Find recent files" },
     },
 }
 
--- nvim/plugins/telescope.lua
+++ nvim/plugins/telescope.lua
@@ -1,5 +1,30 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require("telescope.builtin")
+
+        vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find files" })
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+        vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+        vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "Search in buffer" })
+        vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+        vim.keymap.set("n", "<leader>km", builtin.keymaps, { desc = "Keymaps" })
+
+        require("telescope").setup {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-k>"] = "move_selection_previous",
+                        ["<C-j>"] = "move_selection_next",
+                        ["<C-q>"] = "close",
+                    },
+                },
+            },
+        }
+    end
 }
